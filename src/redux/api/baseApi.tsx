a4fd76faac3a5139fa0dda8517b349243df363ff
Md. Rahad Ullah <rahadullah10@gmail.com>
2025-04-11 23:53:15 +0600

    Updated server base url

--- src/redux/api/baseApi.tsx
+++ src/redux/api/baseApi.tsx
@@ -5,7 +5,7 @@ export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
     // baseUrl: "http://localhost:5000/api/v1",
-    baseUrl: "https://bazarly-server.vercel.app/api/v1",
+    baseUrl: "https://bazarly-server.vercel.app/api/v1/fdakj",
     credentials: "include",
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).auth.accessToken;
